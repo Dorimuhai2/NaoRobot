d073f7d4f260fa2a9147aed51b00c41bc02adeba
Dorimuhai <87837653+Dorimuhai@users.noreply.github.com>
2021-11-11 01:30:23 +0700

    Update alive(1).py

--- NaoRobot/modules/alive(1).py
+++ NaoRobot/modules/alive(1).py
@@ -12,11 +12,11 @@ PHOTO = "https://telegra.ph/file/93b07a50496c85afbdd51.jpg"
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Keyzia!** \n\n"
-  LUNA += "💎 **I'm Working Properly** \n\n"
-  LUNA += "💎 **My Master : [ꜱᴇᴛʜ★](https://t.me/xyzseth)** \n\n"
-  LUNA += f"💎 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"💎 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Thanks For Adding Me Here ❤️**"
+  KEYZIA = "**Holla I'm Keyzia!** \n\n"
+  KEYZIA += "💎 **I'm Working Properly** \n\n"
+  KEYZIA += "💎 **My Master : [ꜱᴇᴛʜ★](https://t.me/xyzseth)** \n\n"
+  KEYZIA += f"💎 **Telethon Version : {tlhver}** \n\n"
+  KEYZIA += f"💎 **Pyrogram Version : {pyrover}** \n\n"
+  KEYZIA += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Keyziarobot_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/sethproject")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=KEYZIA,  buttons=BUTTON)
